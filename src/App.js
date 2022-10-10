bd6214a7a8e7e97f0c33aea97b7937044b64b6df
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-10-11 03:03:06 +0600

    Some changed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import UserList from './Features/Users/UserList';
 
 function App() {
   return (
-    <div>
-      <UserList></UserList>
+    <div className='container mx-auto px-2 max-w-5xl pt-10 md:pt-32'>
+      <h1 className='text-center font-bold text-2xl text-gray-700'>CRUD Operation with Redux Toolkit</h1>
+      <UserList />
     </div>
   );
 }
